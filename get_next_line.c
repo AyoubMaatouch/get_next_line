7416b6ded123bc1fb3e40ad679ae75e9e52141b1
Ayoub Maatouch <aymaatou@e1r1p11.1337.ma>
2019-11-13 20:46:18 +0100

    O~O

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aymaatou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/10 20:39:05 by aymaatou          #+#    #+#             */
-/*   Updated: 2019/11/13 17:39:49 by aymaatou         ###   ########.fr       */
+/*   Updated: 2019/11/13 20:45:46 by aymaatou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -15,39 +15,41 @@
 
 int get_next_line(int fd, char **line)
 {	
-	static char *temp;
+	 char *temp;
 	static char *res;
 	int	r_value, i = 0;
-
+	static int find;
 	r_value = 0;
 
 	*line = NULL;
+	//puts(res);
 	if (res)
 	{
+	//	printf ("res %d \n %s", find++, res);
 		//printf("============>leftover : %s\n", res);
 		while (res[i] != '\n' && res[i] != '\0')
 			i++;
 
 		//printf("aftre : %s\n", ft_substr(temp, 0, i));
 		*line = ft_strjoin(*line, ft_substr(res,0, i));
-		res = ft_strchr(res, '\n') + 1;
-		/*if (res[i] == '\n')	
-		{	puts("enter");*/
+		//printf("line  %s\n", *line);
+		//puts(res);
+		if (res[i] == '\n')
+		{
+			res = ft_strchr(res, '\n') + 1;
 			return (1);
-	//	}
+		}
 	}
 	temp = malloc(BUFFER_SIZE + 1);
 	r_value = read (fd, temp, BUFFER_SIZE);
 	if (r_value == 0)
+	{	*line = NULL;
 		return (0);
+	}
 	while (temp[i] != '\n' && temp[i] != '\0')
 		i++;
-		printf("value temp \n%s\n", temp);
-	//printf("aftre : %s\n", ft_substr(temp, 0, i));
 	*line = ft_strjoin(*line, ft_substr(temp,0, i));
-	//printf("line : %s\n", *line);
 	res = ft_strchr(temp, '\n') + 1;
-	//puts(temp);
 	//free (temp);
 	return (1);
 }
@@ -60,12 +62,11 @@ int    main(void)
 
 	n = 0;
 	line = malloc(BUFFER_SIZE + 1);
-	while (n++ < 10) 
+	while (n++ < 25) 
 	{
 		num = get_next_line(i, &line);
-		printf( "(%d)==(%d)===> %s\n" ,n, num, line);
+		printf( "\n(%d)==(%d)===> %s\n" ,n, num, line);
 	}
-	free(line);
 	return (0);
 }
 
