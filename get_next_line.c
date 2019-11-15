d97389cbf7371017538f673110d0edee1a9685dd
Ayoub Maatouch <aymaatou@e1r10p2.1337.ma>
2019-11-15 22:05:37 +0100

    +++

--- get_next_line.c
+++ get_next_line.c
@@ -3,107 +3,138 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aymaatou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: aymaatou <aymaatou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/10 20:39:05 by aymaatou          #+#    #+#             */
-/*   Updated: 2019/11/14 15:38:14 by aymaatou         ###   ########.fr       */
+/*   Updated: 2019/11/15 22:01:55 by aymaatou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 
 #if defined BUFFER_SIZE
 
-int b_check(char *temp)
+int s_strlen(char *r_buffer)
+{
+	int size;
+
+	size = 0;
+	while (r_buffer[size] != '\n' && r_buffer[size] != '\0')
+		size++;
+	return(size);
+}
+int	check(char *r_buffer)
 {
 	int i;
 
 	i = 0;
-	while (temp[i] != '\n' && temp[i] != '\0')
+	while (r_buffer[i])
+	{
+		if (r_buffer[i] == '\n')
+			return (0);
 		i++;
-	if (temp[i] == '\n')
-		return (0);
-	else
-		return (1);
+	}
+	return(1);
 }
 
-/*void	res_check(char *res)
+int	check_end(char *r_buffer)
 {
-	if (res)
-	{
-		while (res[i] != '\n' && res[i] != '\0')
-			i++;
-		*line = ft_strjoin(*line, ft_substr(res,0, i));
-		if (res[i] == '\n')
-		{
-			res = ft_strchr(res, '\n') + 1;
-			return (1);
-		}
-	}
-}*/
+	int i;
 
-int get_next_line(int fd, char **line)
-{	
-	char *temp;
-	static char *res;
-	int	r_value;
-	int	i;
-   	int  stop;
-   
-	stop = 1;
 	i = 0;
-	r_value = 0;
-	*line = NULL;
-	temp = malloc(BUFFER_SIZE + 1);
-	if (BUFFER_SIZE == 0 || BUFFER_SIZE < 0)
-		return (-1);
-	if (res)
+	while (i < BUFFER_SIZE)
 	{
-		while (res[i] != '\n' && res[i] != '\0')
-			i++;
-		*line = ft_strjoin(*line, ft_substr(res,0, i));
-		if (res[i] == '\n')
-		{
-			res = ft_strchr(res, '\n') + 1;
-			if (res[0] == 0)
-				return (0);
-			return (1);
-		}
+		if (r_buffer[i] == '\n' && r_buffer[i + 1] == '\0')
+			return(0);
+		i++;
 	}
-
-	while (stop)
-	{
-		if ((r_value = read (fd, temp, BUFFER_SIZE)) < 0)
-			return (-1);
-		temp[r_value] = '\0';	
-		stop = b_check(temp);
-		while (temp[i] != '\n' && temp[i] != '\0')
-			i++;
-		*line = ft_strjoin(*line, ft_substr(temp,0, i));
-		if (r_value == 0)
-			return (0);
-	}
-	res = ft_strdup("");
-	res = ft_strchr(temp, '\n') + 1;
-	free(temp);
 	return (1);
 }
+
+int	freedom(char *box, char *r_buffer)
+{
+	//free(box);
+	box = NULL;
+	//free(r_buffer);
+	r_buffer = NULL;
+	return (0);
+	
+
+}
+int get_next_line(int fd, char **line)
+{
+	int			r_value;
+	int			i;
+	char		*r_buffer;
+	static char *box;
+
+	i = 1;
+	*line = NULL;
+	//static int st;	
+	while (1)
+	{
+		
+		if (box)
+		{
+			if (!check(box))
+			{
+
+			//printf("%d box %s\n", st++, box);	
+
+				
+				*line = ft_strjoin(*line, box);
+				box = ft_strdup(&box[ft_strchr(box, '\n') + 1]); 	
+				
+			//	printf("box value: %s\n", box);
+				return (1);
+			}
+			else
+			{	
+				*line = ft_strdup(box);
+				free(box);
+			}
+		}
+
+
+		
+		while (i) 
+		{	r_buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+			r_value = read(fd, r_buffer, BUFFER_SIZE);
+			r_buffer[r_value] = '\0';
+			i = check(r_buffer);
+			*line = ft_strjoin(*line, r_buffer);
+			if (r_value == 0)
+			{
+				box = NULL;
+			//	free(r_buffer);
+			//	r_buffer = NULL;		
+				return (freedom(box, r_buffer));
+				
+			}
+			
+			if (i == 0)
+				box = ft_strdup(&r_buffer[ft_strchr(r_buffer, '\n') + 1]);
+		}
+		free(r_buffer);	
+		return (1);
+	}
+
+}
+
 int    main(void)
 {
 
-	int i = open("txt.txt", O_RDONLY);
+	int i = open("normal.txt", O_RDONLY);
 	char *line;
-	int n , num = 1;
+	int n, num = 1;
 
-	n = 1;
-	line = malloc(BUFFER_SIZE + 1);
-	while (num) 
+	while(num)
 	{
 		num = get_next_line(i, &line);
-		printf( "\n(%d)==(%d)===> %s\n" ,n, num, line);
-		
-		n++;
+		printf( "(%d)===> %s\n" , num, line);
+		free(line);
 	}
 	return (0);
+	
 }
 
 #endif
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: aymaatou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 15:17:28 by aymaatou          #+#    #+#             */
-/*   Updated: 2019/11/14 14:42:17 by aymaatou         ###   ########.fr       */
+/*   Updated: 2019/11/15 21:32:58 by aymaatou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,11 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-char	*ft_strjoin(char const *s1 ,char const *s2);
+int s_strlen(char *r_buffer);
+char	*ft_strjoin(char *s1 ,char *s2);
+char	*ft_strjoin_box(char *s1 ,char *s2);
 size_t	ft_strlen(const char *str);
-char	*ft_strchr(const char *str, int c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strdup(const char *s1);
+int		ft_strchr(const char *str, int c);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+char	*ft_strdup(char *s1);
+int get_next_line(int fd, char **line);
